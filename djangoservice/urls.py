0a1e60d857e8034da2fda2af08560b8e78852d07
Lorelei Basken <lorelei@basken.com>
2024-09-15 19:37:32 -0400

    fixed incorrect file reference in admin urls.py

--- djangoservice/urls.py
+++ djangoservice/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("api/", include("app.urls")),
+    path("api/", include("item.urls")),
     path('admin/', admin.site.urls),
 ]
