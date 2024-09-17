f24197eca309bc95f4948ffbea87fdfb20e13e3c
Lorelei Basken <lorelei@basken.com>
2024-09-17 19:41:27 -0400

    CRUDlist functionality

--- item/models.py
+++ item/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Item(models.Model):
+    DoesNotExist = None
+    objects = None
     title = models.CharField(max_length=200)
     date = models.DateTimeField("test date")
-    text = models.TextField(max_length=200)
+    description = models.TextField(max_length=200)
--- item/views.py
+++ item/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed, Http404
 from .serializers import ItemSerializer
+import json
 from .models import Item
 
 
@@ -7,3 +8,58 @@ def get_all_items(request):
     query_set = Item.objects.all()
     serializer = ItemSerializer(query_set, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+def create_new_item(request):
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+        new_title = body.get("title")
+        new_date = body.get("date")
+        new_description = body.get("description")
+
+        new_item = Item(name=new_title, address=new_date, latitude=new_description)
+        new_item.save()
+
+        return HttpResponse(status=200)
+    else:
+        raise HttpResponseNotAllowed("Method is not supported")
+
+
+def update_item_by_id(request, item_id):
+    if request.method == 'PUT':
+        body = json.loads(request.body.decode('utf-8'))
+        new_title = body.get("title")
+        new_date = body.get("date")
+        new_description = body.get("description")
+        try:
+            item = Item.objects.get(pk=item_id)
+        except Item.DoesNotExist:
+            raise Http404("Restaurant does not exist")
+        item.title = new_title
+        item.date = new_date
+        item.description = new_description
+        item.save()
+        return HttpResponse(status=200)
+    else:
+        raise HttpResponseNotAllowed("Method is not supported")
+
+
+def delete_item_by_id(request, item_id):
+    if request.method == 'DELETE':
+        try:
+            item = Item.objects.get(pk=item_id)
+            item.delete()
+            return HttpResponse(status=200)
+        except Item.DoesNotExist:
+            raise Http404("Restaurant does not exist")
+    else:
+        raise HttpResponseNotAllowed("Method is not supported")
+
+
+def get_item_by_id(request, item_id):
+    try:
+        query_set = Item.objects.filter(pk=item_id)
+        serializer = ItemSerializer(query_set.get())
+        return JsonResponse(serializer.data, safe=False)
+    except Item.DoesNotExist:
+        raise Http404("Restaurant does not exist")
