2adfa26f9004bfc1c537a91f3c98109615c1d8b5
Lorelei Basken <lorelei@basken.com>
2024-09-20 19:29:13 -0400

    added endpoints

--- item/urls.py
+++ item/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('item/', views.get_all_items, name='get_all_items'),
+    path('item/new/', views.create_new_item, name='create_new_item'),
+    path('item/<int:restaurant_id>/', views.get_item_by_id, name='get_item_by_id'),
+    path('item/patch/<int:restaurant_id>/', views.update_item_by_id, name='update_item_by_id'),
+    path('item/delete/<int:restaurant_id>/', views.delete_item_by_id, name='delete_item_by_id'),
 ]
